86559d742c65ea44f23154ecdf9b4196dd281cd0
Rufael Marew <rufaelfekadu48@gmail.com>
2023-08-30 18:34:36 +0400

    adverserial training updated

--- utils.py
+++ utils.py
@@ -98,6 +98,7 @@ def arg_parse():
     parser.add_argument('--thr_domain', type=float, default=0.87)
     parser.add_argument('--num_val', type=int, default=3)  # number of val. within each epoch
     parser.add_argument('--save_freq', type=int, default=5)
+    parser.add_argument('--n_splits', type=int, default=5)
     # misc
     parser.add_argument('--device', type=str, default='cuda')
     parser.add_argument('--n_workers', type=int, default=4)
