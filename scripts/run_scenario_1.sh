2132855db4ba6ee8b6006a4c781710eabc92e461
Rufael Marew <rufaelfekadu48@gmail.com>
2023-09-03 11:56:58 +0400

    minor fix

--- scripts/run_scenario_1.sh
+++ scripts/run_scenario_1.sh
@@ -4,7 +4,8 @@ OUTPUT_DIR = "./output/scenario_1"
 #run the training script
 for subject in 1 4 5 7 11 12 15 16
 do
-    python train.py DATA.DIR $DATA_DIR OUTPUT.LOG_DIR $OUTPUT_DIR DATA.SUBJECT $subject
+    python train.py DATA.DIR $DATA_DIR OUTPUT.LOG_DIR $OUTPUT_DIR DATA.SUBJECT $subject \
+    TRAIN.BATCH_SIZE 64 TRAIN.EPOCHS 100 TRAIN.LR 0.001 
 done
 
 #run train_adv
